93694eabf5c38545e6ad9cb38dca7d9b2296b1d9
poisonpotato <109737372+poisonthepower@users.noreply.github.com>
2023-06-27 13:14:21 +0600

    VC: multiple token supported

--- main.py
+++ main.py
@@ -6,48 +6,86 @@ import requests
 import websocket
 from keep_alive import keep_alive
 
-status = "online" #online/dnd/idle
+status = "dnd"  # online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
-SELF_MUTE = True
+GUILD_ID = 1112644273725259807
+CHANNEL_ID = 1112655603102396436
+SELF_MUTE = False
 SELF_DEAF = False
 
-usertoken = os.getenv("TOKEN")
-if not usertoken:
-  print("[ERROR] Please add a token inside Secrets.")
-  sys.exit()
+usertokens = [
+    os.getenv("TOKEN1"),
+    os.getenv("TOKEN2"),
+    os.getenv("TOKEN3"),
+    # Add more token variables if needed
+]
 
-headers = {"Authorization": usertoken, "Content-Type": "application/json"}
+headers = {"Content-Type": "application/json"}
 
-validate = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers)
-if validate.status_code != 200:
-  print("[ERROR] Your token might be invalid. Please check it again.")
-  sys.exit()
-
-userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
-username = userinfo["username"]
-discriminator = userinfo["discriminator"]
-userid = userinfo["id"]
+def validate_token(token):
+    headers["Authorization"] = token
+    validate = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers)
+    return validate.status_code == 200
 
 def joiner(token, status):
     ws = websocket.WebSocket()
     ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
     start = json.loads(ws.recv())
     heartbeat = start['d']['heartbeat_interval']
-    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
-    vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
+    auth = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": "Windows 10",
+                "$browser": "Google Chrome",
+                "$device": "Windows"
+            },
+            "presence": {
+                "status": status,
+                "afk": False
+            }
+        },
+        "s": None,
+        "t": None
+    }
+    vc = {
+        "op": 4,
+        "d": {
+            "guild_id": GUILD_ID,
+            "channel_id": CHANNEL_ID,
+            "self_mute": SELF_MUTE,
+            "self_deaf": SELF_DEAF
+        }
+    }
     ws.send(json.dumps(auth))
     ws.send(json.dumps(vc))
     time.sleep(heartbeat / 1000)
-    ws.send(json.dumps({"op": 1,"d": None}))
+    ws.send(json.dumps({"op": 1, "d": None}))
 
 def run_joiner():
-  os.system("clear")
-  print(f"Logged in as {username}#{discriminator} ({userid}).")
-  while True:
-    joiner(usertoken, status)
-    time.sleep(30)
+    os.system("clear")
+    token_count = len(usertokens)
+    if token_count == 0:
+        print("[ERROR] Please add tokens inside Secrets.")
+        sys.exit()
+
+    valid_tokens = [token for token in usertokens if validate_token(token)]
+    if len(valid_tokens) == 0:
+        print("[ERROR] All tokens are invalid. Please check them again.")
+        sys.exit()
+
+    print(f"Successfully validated {len(valid_tokens)}/{token_count} tokens.")
+    for i, token in enumerate(valid_tokens, start=1):
+        headers["Authorization"] = token
+        userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
+        username = userinfo["username"]
+        discriminator = userinfo["discriminator"]
+        userid = userinfo["id"]
+        print(f"Logged in as {username}#{discriminator} ({userid}).")
+        joiner(token, status)
+        print(f"Token {i}/{len(valid_tokens)} finished. Reconnecting in 30 seconds...")
+        time.sleep(30)
 
 keep_alive()
 run_joiner()
