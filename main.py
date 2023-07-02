7aa3caa5ac6de8fdd859dc8c4f0c72ee0f2e5384
bhaiamar <116642981+bhaiamar@users.noreply.github.com>
2023-07-02 13:08:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = 1112644273725259807
-CHANNEL_ID = 1112655603102396436
+GUILD_ID = 1029698252632752198
+CHANNEL_ID = 1053585168797024286
 SELF_MUTE = True
 SELF_DEAF = False
 
 # Add your tokens here as a list of strings
-usertokens = [os.getenv("TOKEN1"), os.getenv("TOKEN2"), os.getenv("TOKEN3")]
+usertokens = [os.getenv("MTExNDgwNDg2MTMzOTQ0MzIwMQ.GxWlUg.kVXaaUdZ4IziECOCbkDgB_TdfzEl5jFTD9eDtI"), os.getenv("TOKEN2"), os.getenv("TOKEN3")]
 
 for usertoken in usertokens:
     if not usertoken:
